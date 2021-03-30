8faa8e48b69f30106cb0b2940f5e4e8b99c613e9
jamaldine <jboukir@gmail.com>
2021-03-30 20:26:02 +0100

    new staructure

--- src/controllers/problemContoller.js
+++ src/controllers/problemContoller.js
@@ -3,12 +3,7 @@ import { Problem } from "../models/Problem";
 import { PROBLEM_CREATED } from "../constants/constants";
 import { pubsub } from "../index";
 
-// Create a new problem from the given input
 export async function createProblem({ title, description, media }) {
-  /*const aproblem = new Problem({ title, description, media });
-    await aproblem.save();
-    return aproblem;*/
-
   const problem = await Problem.create({
     title,
     description,
--- src/models/Problem.js
+++ src/models/Problem.js
@@ -1,20 +1,12 @@
 import mongoose from "mongoose";
 
-// src/models/card/model.ts
-
 const Schema = new mongoose.Schema(
-    {
-        title: String,
-        description: String,
-        media: String,
-    },
-    // Adds createdAt and updatedAt to the model
-    { timestamps: true }
-)
+  {
+    title: String,
+    description: String,
+    media: String,
+  },
+  { timestamps: true }
+);
 
-export const Problem =  mongoose.model('Problem', Schema)
-
-/**
- * other way
- * export const Problem = mongoose.model("Problem", { title: String, description: String, media: String });
- */
+export const Problem = mongoose.model("Problem", Schema);
