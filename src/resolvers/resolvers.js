d69bf8589dbced748a7b34682a90d7ff22c06367
jamaldine <jboukir@gmail.com>
2021-03-30 19:51:41 +0100

    remove solver because of accounts-js

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,19 +1,12 @@
 import { pubsub } from "../index";
 import { Problem } from "../models/Problem";
-import { Solver } from "../models/Solver";
 
-import { PROBLEM_CREATED, SOLVER_CREATED } from "../constants/constants";
+import { PROBLEM_CREATED } from "../constants/constants";
 import {
   getAllProblems,
   GetAllProblemsInput,
   createProblem,
 } from "../controllers/problemContoller";
-
-import {
-  getAllSolvers,
-  GetAllSolversInput,
-  createSolver,
-} from "../controllers/solverController";
 
 export const resolvers = {
   Subscription: {
@@ -21,22 +14,14 @@ export const resolvers = {
       // Additional event labels can be passed to asyncIterator creation
       subscribe: () => pubsub.asyncIterator([PROBLEM_CREATED]),
     },
-    SolverCreated: {
-      subscribe: () => pubsub.asyncIterator([SOLVER_CREATED]),
-    },
   },
   Query: {
     problems: (_, { input: GetAllProblemsInput }) =>
       getAllProblems({ limit: 0 }),
-
-    solvers: (_, { input: GetAllSolversInput }) => getAllSolvers({ limit: 0 }),
   },
   Mutation: {
     CreateProblem: async (_, { title, description, media }) =>
       await createProblem({ title, description, media }),
-
-    CreateSolver: async (_, { name, lastname, avatar, email, password }) =>
-      await createSolver({ name, lastname, avatar, email, password }),
   },
 };
 
