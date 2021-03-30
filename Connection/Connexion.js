3d3bfac927639c23757c5cab24410090a3384f32
jamaldine <jboukir@gmail.com>
2021-03-30 19:09:01 +0100

    auth done

--- Connection/Connexion.js
+++ Connection/Connexion.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+const { Mongo } = require("@accounts/mongo");
 
 const connectDB = async () => {
   await mongoose.connect("mongodb://localhost:27017/problemSolver", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
+  // We tell accounts-js to use the mongo connection
+  const accountsMongo = new Mongo(mongoose.connection);
 };
 module.exports = connectDB;
